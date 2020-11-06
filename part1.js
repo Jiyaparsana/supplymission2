b0b73e0e46275c14bb0d73476adee847cb50b51a
Jiyaparsana <73345190+Jiyaparsana@users.noreply.github.com>
2020-11-06 20:11:34 +0530

    Add files via upload

---
+++ part1.js
@@ -0,0 +1,17 @@
+class Part1{
+    constructor(){
+
+this.body = Bodies.rectangle(400,640,20,100);
+this.width = 20;
+this.height = 100;
+World.add(world, this.body);
+
+    }
+    display(){
+RECTmode(CENTER)
+var pos = this.Bodies.position
+rectangle(pos.x,pos.y,this.width,this.height)
+this.body.shapeColor= "red"
+
+    }
+}
